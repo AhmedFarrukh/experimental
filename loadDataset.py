374ccfb28d0be30e6797980ad9a0a5240a87552b
AhmedFarrukh <101267023+AhmedFarrukh@users.noreply.github.com>
2024-07-19 17:25:22 +0500

    Update loadDataset.py

--- loadDataset.py
+++ loadDataset.py
@@ -3,8 +3,9 @@ import os
 import tensorflow_datasets as tfds
 
 ds_name = 'imagenet2012'
-[images, labels], info = tfds.load(ds_name, split='validation', as_supervised=True, with_info=True)
+ds, info = tfds.load(ds_name, split='validation', as_supervised=True, with_info=True)
 print(len(ds))
 
-for i in range(5):
-  print(labels[i])
+for image, label in ds:
+  print(label)
+  break
