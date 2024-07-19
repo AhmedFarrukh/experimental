5ebe76576da3c60938fc6dec9a49835e1a04c20b
AhmedFarrukh <101267023+AhmedFarrukh@users.noreply.github.com>
2024-07-19 20:05:54 +0500

    Update measuringAccuracy.py

--- measuringAccuracy.py
+++ measuringAccuracy.py
@@ -7,42 +7,49 @@ ds_name = 'imagenet2012'
 ds, info = tfds.load(ds_name, split='validation', as_supervised=True, with_info=True)
 
 def inference(model_path, modelName):
-  tflite_model_path = model_path
-  interpreter = tf.lite.Interpreter(model_path=tflite_model_path)
-  interpreter.allocate_tensors()
+    tflite_model_path = model_path
+    interpreter = tf.lite.Interpreter(model_path=tflite_model_path)
+    interpreter.allocate_tensors()
 
-  input_index = interpreter.get_input_details()[0]["index"]
-  output_index = interpreter.get_output_details()[0]["index"]
+    input_index = interpreter.get_input_details()[0]["index"]
+    output_index = interpreter.get_output_details()[0]["index"]
 
-  correct_predictions = 0
-  total_predictions = 0
+    correct_predictions = 0
+    total_predictions = 0
 
-  preProcessDetails = {"MobileNet": ([224, 224], "mobilenet"),
-                     "ResNet50": ([224, 224], "resnet"),
-                     "ResNet101": ([224, 224], "resnet"),
-                     "InceptionV3": ([299, 299], "inception_v3"),
-                     "VGG16": ([224, 224], "vgg16"),
-                     "VGG19": ([224, 224], "vgg19"),
-                     "ResNet152": ([224, 224], "resnet")}
-  def preprocess(image, label):
-      image = tf.image.resize(image, preProcessDetails[modelName][0])
-      model = getattr(tf.keras.applications, preProcessDetails[modelName][1])
-      image = model.preprocess_input(image)
-      return image, label
+    preProcessDetails = {
+        "MobileNet": ([224, 224], "mobilenet"),
+        "ResNet50": ([224, 224], "resnet"),
+        "ResNet101": ([224, 224], "resnet"),
+        "InceptionV3": ([299, 299], "inception_v3"),
+        "VGG16": ([224, 224], "vgg16"),
+        "VGG19": ([224, 224], "vgg19"),
+        "ResNet152": ([224, 224], "resnet")
+    }
 
-  ds_processed = ds.map(preprocess).batch(1)
+    def preprocess(image, label):
+        image = tf.image.resize(image, preProcessDetails[modelName][0])
+        model = getattr(tf.keras.applications, preProcessDetails[modelName][1])
+        image = model.preprocess_input(image)
+        return image, label
 
-  for image, label in ds_processed:
-    interpreter.set_tensor(input_index, image)
-    interpreter.invoke()
-    predictions = interpreter.get_tensor(output_index)
-    if np.argmax(predictions[0]) == label.numpy()[0]:
-      correct_predictions += 1
-    total_predictions += 1
+    # Batch processing and prefetching
+    batch_size = 32  # Adjust the batch size depending on your system's capabilities
+    ds_processed = ds.map(preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE).batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE)
 
-  return correct_predictions/total_predictions
+    for images, labels in ds_processed:
+        interpreter.set_tensor(input_index, images.numpy())
+        interpreter.invoke()
+        predictions = interpreter.get_tensor(output_index)
+        correct_predictions += np.sum(np.argmax(predictions, axis=1) == labels.numpy())
+        total_predictions += labels.shape[0]
+
+    return correct_predictions / total_predictions
+
 modelNames = ["MobileNet", "ResNet50", "ResNet101", "InceptionV3", "VGG16", "VGG19", "ResNet152"]
 for modelName in modelNames:
-  print(modelName + ": ", inference("./tflite_models/" + modelName +".tflite", modelName))
-  print(modelName + " Quantized: ", inference("./tflite_models/" + modelName +"_quant.tflite", modelName))
+    accuracy = inference("./tflite_models/" + modelName + ".tflite", modelName)
+    print(f"{modelName}: {accuracy:.2%}")
+    quant_accuracy = inference("./tflite_models/" + modelName + "_quant.tflite", modelName)
+    print(f"{modelName} Quantized: {quant_accuracy:.2%}")
 
