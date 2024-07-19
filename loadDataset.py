a0cdb8373cc644ef4ec7ff53adbb760d3dc37406
AhmedFarrukh <101267023+AhmedFarrukh@users.noreply.github.com>
2024-07-19 17:18:10 +0500

    Update loadDataset.py

--- loadDataset.py
+++ loadDataset.py
@@ -3,5 +3,5 @@ import os
 import tensorflow_datasets as tfds
 
 ds_name = 'imagenet2012'
-ds, info = tfds.load(ds_name, split='validation', as_supervised=True, with_info=True, data_dir='~/tensorflow_datasets/downloads/manual/imagenet2012')
+ds, info = tfds.load(ds_name, split='validation', as_supervised=True, with_info=True)
 print(len(ds))
