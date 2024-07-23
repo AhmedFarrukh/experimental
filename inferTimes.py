28d996dc6d44767752835ed93e15303ee106d178
AhmedFarrukh <101267023+AhmedFarrukh@users.noreply.github.com>
2024-07-23 18:34:33 +0500

    Update inferTimes.py

--- inferTimes.py
+++ inferTimes.py
@@ -3,15 +3,15 @@ modelNames = ["MobileNet", "InceptionV3", "ResNet50", "ResNet101" , "ResNet152",
 file = open("/root/results.txt", "w")
 
 numModels = len(modelNames)
-allModels = modelNames.copy()
 for i in range(numModels):
-  allModels.append(modelNames[i] + "_quant")
+  modelNames.append(modelNames[i] + "_quant")
 
-for modelName in allModels:
+for modelName in modelNames:
     outputOriginal = subprocess.check_output("/root/benchmark/linux_aarch64_benchmark_model \
       --graph=/root/tflite_models/" + modelName +".tflite"+" \
       --num_threads=1", shell=True)
     outputOriginal = outputOriginal.decode('utf-8')
+    file.write(modelName + '\n')
     file.write(outputOriginal)
     file.write("\n")
 
