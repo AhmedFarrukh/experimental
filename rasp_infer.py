a40978ddcc15f834e519af14179431082f93d15d
AhmedFarrukh <101267023+AhmedFarrukh@users.noreply.github.com>
2024-07-26 04:23:04 +0500

    Update rasp_infer.py

--- rasp_infer.py
+++ rasp_infer.py
@@ -7,13 +7,13 @@ for i in range(numModels):
 
 for modelName in modelNames:
   file = open(f'/home/pi/DeepLearningEdgeComputing/results/{modelName}.txt', "w")
-    for i in range(10):
-      outputOriginal = subprocess.check_output("/home/pi/DeepLearningEdgeComputing/benchmark/linux_aarch64_benchmark_model \
-        --graph=/home/pi/DeepLearningEdgeComputing/tflite_models/" + modelName +".tflite"+" \
-        --num_threads=1", shell=True)
-      outputOriginal = outputOriginal.decode('utf-8')
-      file.write(modelName + '\n')
-      file.write(outputOriginal)
-      file.write("\n")
+  for i in range(10):
+    outputOriginal = subprocess.check_output("/home/pi/DeepLearningEdgeComputing/benchmark/linux_aarch64_benchmark_model \
+      --graph=/home/pi/DeepLearningEdgeComputing/tflite_models/" + modelName +".tflite"+" \
+      --num_threads=1", shell=True)
+    outputOriginal = outputOriginal.decode('utf-8')
+    file.write(modelName + '\n')
+    file.write(outputOriginal)
+    file.write("\n")
   file.close()
     
