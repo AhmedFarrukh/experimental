233c7d980f34164d1ad0dfd0bcba86d7e23fcab1
AhmedFarrukh <101267023+AhmedFarrukh@users.noreply.github.com>
2024-07-26 03:15:41 +0500

    Update inferTimes.py

--- inferTimes.py
+++ inferTimes.py
@@ -7,13 +7,14 @@ for i in range(numModels):
   modelNames.append(modelNames[i] + "_quant")
 
 for modelName in modelNames:
-    outputOriginal = subprocess.check_output("/root/benchmark/linux_aarch64_benchmark_model \
-      --graph=/root/tflite_models/" + modelName +".tflite"+" \
-      --num_threads=1", shell=True)
-    outputOriginal = outputOriginal.decode('utf-8')
-    file.write(modelName + '\n')
-    file.write(outputOriginal)
-    file.write("\n")
-
-file.close()
+  file = open(f'/root/{modelName}.txt', "w")
+    for i in range(10):
+      outputOriginal = subprocess.check_output("/root/benchmark/linux_aarch64_benchmark_model \
+        --graph=/root/tflite_models/" + modelName +".tflite"+" \
+        --num_threads=1", shell=True)
+      outputOriginal = outputOriginal.decode('utf-8')
+      file.write(modelName + '\n')
+      file.write(outputOriginal)
+      file.write("\n")
+  file.close()
     
