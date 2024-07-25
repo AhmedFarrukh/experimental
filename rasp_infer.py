4a92e2b87ca1a4a559c88222ae7d750924d28d56
AhmedFarrukh <101267023+AhmedFarrukh@users.noreply.github.com>
2024-07-26 03:53:49 +0500

    Create rasp_infer.py

---
+++ rasp_infer.py
@@ -0,0 +1,19 @@
+import subprocess
+modelNames = ["MobileNet", "InceptionV3", "ResNet50", "ResNet101" , "ResNet152", "VGG16", "VGG19"]
+
+numModels = len(modelNames)
+for i in range(numModels):
+  modelNames.append(modelNames[i] + "_quant")
+
+for modelName in modelNames:
+  file = open(f'/home/pi/DeepLearningEdgeComputing/results/{modelName}.txt', "w")
+    for i in range(10):
+      outputOriginal = subprocess.check_output("/home/pi/DeepLearningEdgeComputing/linux_aarch64_benchmark_model \
+        --graph=/home/pi/DeepLearningEdgeComputing/tflite_models/" + modelName +".tflite"+" \
+        --num_threads=1", shell=True)
+      outputOriginal = outputOriginal.decode('utf-8')
+      file.write(modelName + '\n')
+      file.write(outputOriginal)
+      file.write("\n")
+  file.close()
+    
